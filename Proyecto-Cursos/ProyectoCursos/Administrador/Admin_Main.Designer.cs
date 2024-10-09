497442d8813e62924446f9200b1fb20baf40d78b
panfletoig <panfletoindiegames@gmail.com>
2024-10-09 11:35:40 -0600

    Carlos Interiano - Commit Ejemplo
    Este commit es solo para ejemplificar como se usa github

--- Proyecto-Cursos/ProyectoCursos/Administrador/Admin_Main.Designer.cs
+++ Proyecto-Cursos/ProyectoCursos/Administrador/Admin_Main.Designer.cs
@@ -28,12 +28,31 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Admin_Main";
+			button1 = new Button();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(66, 48);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 23);
+			button1.TabIndex = 0;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// Admin_Main
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(button1);
+			Name = "Admin_Main";
+			Text = "Admin_Main";
+			ResumeLayout(false);
 		}
 
 		#endregion
+
+		private Button button1;
 	}
 }
