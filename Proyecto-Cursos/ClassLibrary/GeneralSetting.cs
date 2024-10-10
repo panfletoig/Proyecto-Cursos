753244b2ba1d5641c8f60111d9fd5be0343f1780
panfletoig <panfletoindiegames@gmail.com>
2024-10-09 20:42:37 -0600

    Clase de GeneralSettings
    Permite agregar herramientas de testeo

---
+++ Proyecto-Cursos/ClassLibrary/GeneralSetting.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace ClassLibrary
+{
+	public class GeneralSetting
+	{
+		#region Test
+		private const bool _Test = true; //Proporciona herramientas para facilitar el desarollo
+		public static bool Test { get { return _Test; } }
+		#endregion
+
+		#region Colors
+		//Colores hexadecimales de la paleta de colores
+		private const string HexCode1 = "#2b85ca";
+		private const string HexCode2 = "#2ba2c8";
+		private const string HexCode3 = "#2bc5c8";
+		private const string HexCode4 = "#9cdfdf";
+		private const string HexCode5 = "#deeded";
+
+		public static Color Blue1 { get{ return ColorTranslator.FromHtml(HexCode1); } }
+		public static Color Blue2 { get{ return ColorTranslator.FromHtml(HexCode2); } }
+		public static Color Blue3 { get{ return ColorTranslator.FromHtml(HexCode3); } }
+		public static Color Blue4 { get{ return ColorTranslator.FromHtml(HexCode4); } }
+		public static Color Blue5 { get{ return ColorTranslator.FromHtml(HexCode5); } }
+		#endregion
+	}
+}
--- Proyecto-Cursos/ProyectoCursos/Form1.Designer.cs
+++ Proyecto-Cursos/ProyectoCursos/Form1.Designer.cs
@@ -32,7 +32,11 @@
 			textBox2 = new TextBox();
 			label_user = new Label();
 			label_pass = new Label();
-			button_ingreso = new Button();
+			btn_ingreso = new Button();
+			btn_admin = new Button();
+			btn_alumno = new Button();
+			btn_profesor = new Button();
+			label_Test = new Label();
 			SuspendLayout();
 			// 
 			// textBox1
@@ -67,27 +71,72 @@
 			label_pass.TabIndex = 3;
 			label_pass.Text = "Contraseña:";
 			// 
-			// button_ingreso
+			// btn_ingreso
 			// 
-			button_ingreso.Location = new Point(84, 217);
-			button_ingreso.Name = "button_ingreso";
-			button_ingreso.Size = new Size(152, 23);
-			button_ingreso.TabIndex = 4;
-			button_ingreso.Text = "Ingresar";
-			button_ingreso.UseVisualStyleBackColor = true;
+			btn_ingreso.Location = new Point(84, 217);
+			btn_ingreso.Name = "btn_ingreso";
+			btn_ingreso.Size = new Size(152, 23);
+			btn_ingreso.TabIndex = 4;
+			btn_ingreso.Text = "Ingresar";
+			btn_ingreso.UseVisualStyleBackColor = true;
+			btn_ingreso.Click += btn_ingreso_Click;
+			// 
+			// btn_admin
+			// 
+			btn_admin.Location = new Point(12, 302);
+			btn_admin.Name = "btn_admin";
+			btn_admin.Size = new Size(90, 23);
+			btn_admin.TabIndex = 5;
+			btn_admin.Text = "Admin";
+			btn_admin.UseVisualStyleBackColor = true;
+			btn_admin.Click += btn_admin_Click;
+			// 
+			// btn_alumno
+			// 
+			btn_alumno.Location = new Point(118, 302);
+			btn_alumno.Name = "btn_alumno";
+			btn_alumno.Size = new Size(90, 23);
+			btn_alumno.TabIndex = 6;
+			btn_alumno.Text = "Alumno";
+			btn_alumno.UseVisualStyleBackColor = true;
+			btn_alumno.Click += btn_alumno_Click;
+			// 
+			// btn_profesor
+			// 
+			btn_profesor.Location = new Point(224, 302);
+			btn_profesor.Name = "btn_profesor";
+			btn_profesor.Size = new Size(88, 23);
+			btn_profesor.TabIndex = 7;
+			btn_profesor.Text = "Profesor";
+			btn_profesor.UseVisualStyleBackColor = true;
+			btn_profesor.Click += btn_profesor_Click;
+			// 
+			// label_Test
+			// 
+			label_Test.AutoSize = true;
+			label_Test.Location = new Point(141, 273);
+			label_Test.Name = "label_Test";
+			label_Test.Size = new Size(40, 15);
+			label_Test.TabIndex = 8;
+			label_Test.Text = "Testeo";
 			// 
 			// Login
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(324, 351);
-			Controls.Add(button_ingreso);
+			Controls.Add(label_Test);
+			Controls.Add(btn_profesor);
+			Controls.Add(btn_alumno);
+			Controls.Add(btn_admin);
+			Controls.Add(btn_ingreso);
 			Controls.Add(label_pass);
 			Controls.Add(label_user);
 			Controls.Add(textBox2);
 			Controls.Add(textBox1);
 			Name = "Login";
 			Text = "Login";
+			Load += Login_Load;
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -98,6 +147,10 @@
 		private TextBox textBox2;
 		private Label label_user;
 		private Label label_pass;
-		private Button button_ingreso;
+		private Button btn_ingreso;
+		private Button btn_admin;
+		private Button btn_alumno;
+		private Button btn_profesor;
+		private Label label_Test;
 	}
 }
